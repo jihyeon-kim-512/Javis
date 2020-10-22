2079d7eb80d41a8f97a8057042c080304f3afc60
jihyeon-kim-512 <73086140+jihyeon-kim-512@users.noreply.github.com>
2020-10-22 13:48:27 +0900

    Update kakao_key.py

--- kakao_key.py
+++ kakao_key.py
@@ -1,30 +1,12 @@
 import requests
 import json
 
-#url = "https://kauth.kakao.com/oauth/token"
-#
-#data = {
-#        "grant_type" : "authorization_code",
-#        "client_id" : "dbbadbe804f295f7da88212ccff2f83f",
-#        "redirect_uri" : "https://localhost.com",
-#        "code" : "qTG-tufJi8CA6hgRqwhQkNGDYihLNy4Wzd2OjXz-KoTCC8i1sHS0rglzifdpkqeYbyytAAopdSkAAAFzjhYhFg"
-#        }
-#
-#response = requests.post(url, data = data)
-#
-#tokens = response.json()
-#
-#with open("kakao_token.json", "w") as fp :
-#    json.dump(tokens,fp)
-#    
-#print(response.json())
-#
 
 url = "https://kauth.kakao.com/oauth/token"
 data = {
     "grant_type" : "refresh_token",
-    "client_id"  : "dbbadbe804f295f7da88212ccff2f83f",
-    "refresh_token" : "dhdwVQt-aI0Dv4ZYXRIrwyrDG2XKegXa0OwyKwo9dJkAAAFzjheEmw"
+    "client_id"  : "",
+    "refresh_token" : ""
 }
 response = requests.post(url, data=data)
 
